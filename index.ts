bffdc170e676ca08288bd4c20bd5ee9f7335ec82
mepavankumar15 <mepavankumar15@gmail.com>
2025-06-06 22:23:52 +0530

    the commit

--- index.ts
+++ index.ts
@@ -7,3 +7,30 @@ const server = new McpServer({
     name: "Avyukth server",
     version: "1.0.0",
 });
+
+server.tool(
+    "getavyus",
+    {
+        fullPrompt : z.string().describe("the complete user query about avyu data"),
+        avyuName: z.string().optional().describe("Optional specific avs name to focus on"),
+    },
+    async ({fullPrompt , avyuName}) => {
+        try{
+            const response = await fetch('https://api.eigenexplorer.com/avs', {
+                headers: {
+                    'X-API-TOKEN': process.env.EIGEN_EXPLORER_API_KEY || '',
+                }
+            })
+
+        } catch (err){
+            return {
+                content: [
+                    {
+                        type:"text",
+                        text: "Error Fetching the data..",
+                    },
+                ],
+            };
+        }
+    }
+)
